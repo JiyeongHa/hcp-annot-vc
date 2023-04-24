75f9d3bf8af26f613ae74b7dd769a65dba5a0855
Jiyeong Ha <jiyeong.ha@nyu.edu>
2023-04-24 15:26:58 -0400

    remove unnecessary variable

--- visualization/plot_contours.py
+++ visualization/plot_contours.py
@@ -66,32 +66,37 @@ def make_trace(rater,
     _display_msg(f'---------------------', True)
     _display_msg(f'subject no.{subject}', True)
     total_start = time.time()
-    if os.path.isfile(cache_path):
-        _display_msg(f'found cache data!', True)
-        (x, y) = ny.load(cache_path)
-    else:
-        annot = vc_plan(rater=rater, sid=subject, hemisphere=hemi, save_path=trace_save_path)
-        _display_msg(f'loading surface mesh....', verbose)
-        start = time.time()
-        cortex = load_cortex(subject, hemi)
-        end = time.time()
-        _display_msg(f'done! elpased time is {np.round(end - start, 3)} sec.\nNow converting trace to path...', verbose)
-        start = time.time()
-        path = roi_trace_to_path(annot, cortex, roi)
-        end = time.time()
-        _display_msg(f'done! elpased time is {np.round(end - start, 3)} sec', verbose)
-        _display_msg('Now transforming the path to fsaverage space...', verbose)
-        start = time.time()
-        fsa_coords = get_path_coords_in_fsaverage(path, cortex)
-        end = time.time()
-        _display_msg(f'done! elpased time is {np.round(end - start, 3)} sec.\nnow interpolating the coordinates..',
-                     verbose)
-        (x, y) = normalize_coords(fsa_coords, n_points)
-        ny.save(cache_path, [x, y])
-    total_end = time.time()
-    _display_msg(f'subject no. {subject} is finished! Elapsed time: {np.round(total_end - total_start, 2)} sec',
-                 True)
+    try:
+        if os.path.isfile(cache_path):
+            _display_msg(f'found cache data!', True)
+            (x, y) = ny.load(cache_path)
+        else:
+            annot = vc_plan(rater=rater, sid=subject, hemisphere=hemi, save_path=trace_save_path)
+            _display_msg(f'loading surface mesh....', verbose)
+            start = time.time()
+            cortex = load_cortex(subject, hemi)
+            end = time.time()
+            _display_msg(f'done! elpased time is {np.round(end - start, 3)} sec.\nNow converting trace to path...', verbose)
+            start = time.time()
+            path = roi_trace_to_path(annot, cortex, roi)
+            end = time.time()
+            _display_msg(f'done! elpased time is {np.round(end - start, 3)} sec', verbose)
+            _display_msg('Now transforming the path to fsaverage space...', verbose)
+            start = time.time()
+            fsa_coords = get_path_coords_in_fsaverage(path, cortex)
+            end = time.time()
+            _display_msg(f'done! elpased time is {np.round(end - start, 3)} sec.\nnow interpolating the coordinates..',
+                         verbose)
+            (x, y) = normalize_coords(fsa_coords, n_points)
+            ny.save(cache_path, [x, y])
+        total_end = time.time()
+        _display_msg(f'subject no. {subject} is finished! Elapsed time: {np.round(total_end - total_start, 2)} sec',
+                     True)
+    except:
+        print(f'{subject} has an error!')
+        x, y = 0, 0
     return x, y
+
 
 
 def main(rater,
