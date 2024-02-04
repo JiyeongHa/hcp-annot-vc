d01533a89a59ef137928e78eea051f25c5ad459b
Jiyeong Ha <jiyeong.ha@nyu.edu>
2024-02-04 12:20:46 -0800

    working on plotting averages across subjects..

--- visualization/utils.py
+++ visualization/utils.py
@@ -9,3 +9,11 @@ def save_fig(save_path):
         if not os.path.exists(parent_path.parent.absolute()):
             os.makedirs(parent_path.parent.absolute())
         plt.savefig(save_path, bbox_inches='tight', transparent=True)
+
+def remove_all_ticks_from_fig(axes):
+    for ax in axes.flatten():
+        ax.set_yticks([])            # Remove y-axis ticks
+        ax.set_yticklabels([])        # Remove y-axis tick labels
+        ax.set_xticks([])            # Remove x-axis ticks
+        ax.set_xticklabels([])        # Remove x-axis tick labels
+ 
